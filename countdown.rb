67437658e6df8a83c7ed3ef87b6e2708fc4dbfbc
w.h.sinn <w.h.sinn@gmail.com>
2019-03-21 02:37:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  number = int
+  while number < 0
+    puts "#{number} SECOND(S)!"
+    number-=
+  end
+    puts "HAPPY NEW YEAR!"
+
 end
